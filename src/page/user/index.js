80b3ab05b62375619bc192020e0dd1332440b511
tangjie <13262879935@163.com>
2019-10-12 14:19:04 +0800

    验收下 大佬

--- src/page/user/index.js
+++ src/page/user/index.js
@@ -9,6 +9,13 @@ import MyAutoScrollBox from '../../components/MyAutoScrollBox'
 
 class User extends React.PureComponent{
 
+    constructor(props){
+        super(props)
+        this.state={
+            num:20
+        }
+    }
+
     getSameLengthArrayByNum=(num)=>{
         const array=[num];
         if (num>0){
@@ -22,17 +29,24 @@ class User extends React.PureComponent{
      return `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
     }
 
+    addDivNum=()=>{
+        this.setState(state=>{
+            return {...state,num:state.num+10}
+        })
+    }
+
     render(){
 
         return (
             <div>
                 <MyAutoScrollBox
                     height={300}
-                    speed={2}
-                    children={this.getSameLengthArrayByNum(20).map((v,k)=>{
+                    speed={5}
+                    children={this.getSameLengthArrayByNum(this.state.num).reverse().map((v,k)=>{
                         return <div style={{height:40,width:'100%',textAlign:'center',lineHeight:'40px',fontSize:20,background:this.getBackgroundColor()}} key={k}>{v}</div>
                     })}
                 />
+                <button onClick={this.addDivNum}>增加10个</button>
             </div>
         )
     }
